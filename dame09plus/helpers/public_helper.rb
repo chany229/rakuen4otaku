4ed9b46c3c32878dffecaf2b87307e118415cb38
chany229 <chany229@sina.com>
2013-07-24 15:34:21 +0800

    .

---
+++ dame09plus/helpers/public_helper.rb
@@ -0,0 +1,7 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+Rakuen4otaku::Dame09plus.helpers do
+  # def simple_helper_method
+  #  ...
+  # end
+end
